9e767d0f3fcf2381ea9392101e5d6eeb3a043826
Charles Wu <charleswu.01@gmail.com>
2020-01-08 16:56:32 -0500

    Scratch the last one

--- CCAT.m
+++ CCAT.m
@@ -29,18 +29,9 @@ debugRect = [10,10, 800,800];
 [screenXpixels, screenYpixels] = Screen('WindowSize', window);
 [xCenter, yCenter] = RectCenter(windowRect);
 picName1 = 'pic1.jpg';
-picName2 = 'pic2.jpg';
-picName3 = 'pic3.jpg';
-picName4 = 'pic4.jpg';
 theImage1 = imread(picName1);
-theImage2 = imread(picName2);
-theImage3 = imread(picName3);
-theImage4 = imread(picName4);
-
 imageTexture1 = Screen('MakeTexture', window, theImage1);
-imageTexture2 = Screen('MakeTexture', window, theImage2);
-imageTexture3 = Screen('MakeTexture', window, theImage3);
-imageTexture4 = Screen('MakeTexture', window, theImage4);
+
 
 
 % We are going to draw four textures to show how a black and white texture
@@ -60,29 +51,12 @@ for i = 1:4
 end
 
 % Batch Draw all of the texures to screen
-Screen('DrawTextures', window,...
-    [imageTexture1, imageTexture2, imageTexture3, imageTexture4],...
-    [], dstRects);
+Screen('DrawTextures', window,imageTexture1,[], dstRects);
 
 % Flip to the screen
 Screen('Flip', window);
 
 
 Screen('CloseAll');
-% Now we have drawn to the screen we wait for a keyboard button press (any
-% key) to terminate the demo.
-KbStrokeWait;
-debugRect = [10,10, 400,400];
-[window, windowRect] = PsychImaging('OpenWindow', screenNumber, grey, debugRect);
-[screenXpixels, screenYpixels] = Screen('WindowSize', window);
-[xCenter, yCenter] = RectCenter(windowRect);
-xPos = linspace(screenXpixels * 0.2, screenXpixels * 0.8, 4);
-yPos = yCenter;
-s1 = 100;
-s2 = 200;
-baseRect = [0 0 s1 s2];
-dstRects = nan(4, 4);
-for i = 1:4
-    dstRects(:, i) = CenterRectOnPointd(baseRect, xPos(i), yPos);
-end
+
 
