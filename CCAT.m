ff61e16f2606d3af32b7c12c11d9e7a82cad10c0
Charles Wu <charleswu.01@gmail.com>
2020-01-08 17:15:36 -0500

    Now I can present the same picture in 12 positions. The next thing is to present 12 different pics in a for loop randomly

--- CCAT.m
+++ CCAT.m
@@ -22,6 +22,9 @@ black = BlackIndex(screenNumber);
 % will be half the luminace values for white
 grey = white / 2;
 
+%%%all experiment perimeters
+nPics = 12;
+
 Screen('Preference', 'SyncTestSettings', 0.001);
 % Open an on screen window using PsychImaging and color it grey.
 debugRect = [10,10, 800,800];
@@ -31,23 +34,23 @@ debugRect = [10,10, 800,800];
 picName1 = 'pic1.jpg';
 theImage1 = imread(picName1);
 imageTexture1 = Screen('MakeTexture', window, theImage1);
-
-
-
-% We are going to draw four textures to show how a black and white texture
-% can be color modulated upon drawing.
-yPos = yCenter;
+% Linearly interpolate the x and y positions based on the current screen
+% size
+yPos = linspace(screenYpixels * 0.2, screenYpixels * 0.8, 3);
 xPos = linspace(screenXpixels * 0.2, screenXpixels * 0.8, 4);
 
 % Define the destination rectangles for our spiral textures. For this demo
 % these will be the same size as out actualy texture, but this doesn't have
 % to be the case. See: ScaleSpiralTextureDemo and CheckerboardTextureDemo.
-s1 = 50;
-s2 = 50;
+s1 = 100;
+s2 = 100;
+
 baseRect = [0 0 s1 s2];
-dstRects = nan(4, 4);
+dstRects = nan(4, nPics);
 for i = 1:4
-    dstRects(:, i) = CenterRectOnPointd(baseRect, xPos(i), yPos);
+    for j = 1:3
+        dstRects(:,(i-1)*3+j) = CenterRectOnPointd(baseRect, xPos(i), yPos(j));
+    end
 end
 
 % Batch Draw all of the texures to screen
